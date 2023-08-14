bfb71bcb1f62213deeeeec8e576ee932936d7e4a
mancabra <mancabra97@gmail.com>
2023-08-14 10:25:30 -0600

    Actualizacion scroll
    se corrigieron los scroll, se agrego el componente crear vacante

--- src/app/Components/Interface/Employer/new-job/new-job.component.ts
+++ src/app/Components/Interface/Employer/new-job/new-job.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empresa } from 'src/app/Services/Entity/empresa';
 
 @Component({
   selector: 'app-new-job',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class NewJobComponent {
 
+  estados:string[] = ["nombre estado","nombre estado",
+  "nombre estado","nombre estado","nombre estado","nombre estado","nombre estado",
+  "nombre estado","nombre estado","nombre estado","nombre estado","nombre estado",
+  "nombre estado","nombre estado","nombre estado","nombre estado","nombre estado",]
+
+  empresasRegistradas: Empresa[] = [];
+  empresaSelecionada: Empresa ;
+
+  constructor(){
+    this.empresaSelecionada = { id_empresa:0, nombre: "Seleccione una empresa", descripcion: "", vacantes_empresa: [] }
+  }
 }
