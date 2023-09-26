0b7c717a2d9db0d5f4a236e71d966632c28435d1
mancabra <mancabra97@gmail.com>
2023-09-26 11:58:32 -0600

    Ultima Act

--- src/app/Services/InterfaceServices/interface.service.ts
+++ src/app/Services/InterfaceServices/interface.service.ts
@@ -168,7 +168,7 @@ export class InterfaceService {
 
   //FUNCION QUE ENVIA UNA ACTUALIZACION A TODOS LOS COMPONENTES QUE SE SUSCRIBIERON A ESTE OBSERVABLE
   hacerVisitante() {
-    this.tipoUsuario = 1;
+    this.tipoUsuario = 0;
     this.tipo$.next(this.tipoUsuario);
   }
 
